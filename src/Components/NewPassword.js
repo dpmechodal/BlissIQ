f5f989840e986c6d0627d075b9d4e76fe835b302
dpmechodal <darshan@mechodal.com>
2024-12-06 18:33:35 +0530

    Darshan add

--- src/Components/NewPassword.js
+++ src/Components/NewPassword.js
@@ -7,26 +7,24 @@ const NewPassword = () => {
     <>
         <Navbar/>
         <div className="container py-100 px-150">
-            <div className="get-start">
-                <div className="row justify-content-center">
-                    <div className="col-lg-8 col-md-7 col-12">
-                        <div className="email-login-text">
-                          <h1>Enter New password</h1>
-                          <p>Enter your new password sent to your Email</p>
-                          <form className='login-form'>
-                            <div className='mb-3'>
-                                <label className='form-label'>Password</label>
-                                <input type="password" className='form-control login-form' placeholder='Enter here...' />
-                            </div>
-                            <div className='mb-3'>
-                                <label className='form-label'>Re-enter password</label>
-                                <input type="password" className='form-control login-form' placeholder='Enter here...' />
-                            </div>
-                            <div>
-                                <Link to={'/'} className='btn btn-c-email w-100'>Continue</Link>
-                            </div>
-                          </form>
+            <div className="get-start bg-white">
+                <div>
+                    <div className="email-login-text text-start">
+                        <h1>Enter New password</h1>
+                        <p>Enter your new password sent to your Email</p>
+                        <form className='login-form'>
+                        <div className='mb-3'>
+                            <label className='form-label'>Password</label>
+                            <input type="password" className='form-control login-form' placeholder='Enter here...' />
                         </div>
+                        <div className='mb-3'>
+                            <label className='form-label'>Re-enter password</label>
+                            <input type="password" className='form-control login-form' placeholder='Enter here...' />
+                        </div>
+                        <div>
+                            <Link to={'/'} className='btn btn-c-email w-100'>Continue</Link>
+                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
